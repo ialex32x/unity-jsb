69a65a4dbe385249d8ecd261a1162e845378047c
huliangjie <lin_032@hotmail.com>
2019-11-25 23:45:17 +0800

    cmake file

--- jsb_build/jsb/jsb.h
+++ jsb_build/jsb/jsb.h
@@ -1,10 +1,15 @@
 
-#if defined(JSB_COMPILING)
-#define JSB_EXTERNAL_DECL extern __declspec(dllexport)
-#define JSB_EXTERNAL __declspec(dllexport)
+#if defined(JSB_F_WINDOWS)
+    #if defined(JSB_COMPILING)
+        #define JSB_EXTERNAL_DECL extern __declspec(dllexport)
+        #define JSB_EXTERNAL __declspec(dllexport)
+    #else
+        #define JSB_EXTERNAL_DECL extern __declspec(dllimport)
+        #define JSB_EXTERNAL
+    #endif
 #else
-#define JSB_EXTERNAL_DECL extern __declspec(dllimport)
-#define JSB_EXTERNAL
+    #define JSB_EXTERNAL_DECL  __attribute__ ((visibility("default"))) extern
+    #define JSB_EXTERNAL       __attribute__ ((visibility("default")))
 #endif
 
 JSB_EXTERNAL_DECL void init();
--- jsb_build/make_jsb_osx.sh
+++ jsb_build/make_jsb_osx.sh
@@ -7,7 +7,6 @@ cd out/osx
 cmake -GXcode ../../jsb
 cd ..
 cmake --build osx --config Release
-# cd ..
-# mkdir -p ./unity/Assets/Duktape/Plugins/duktape.bundle/Contents/MacOS/
-# cp ./build/osx/Release/duktape.bundle/Contents/MacOS/duktape ./unity/Assets/Duktape/Plugins/duktape.bundle/Contents/MacOS/
-
+cd ..
+mkdir -p ../Assets/jsb/Plugins/jsb.bundle/Contents/MacOS/
+cp ./build/osx/Release/jsb.bundle/Contents/MacOS/jsb ../Assets/jsb/Plugins/jsb.bundle/Contents/MacOS/
