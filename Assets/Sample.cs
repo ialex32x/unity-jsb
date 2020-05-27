971ab0ad54643b6ca8b99df378cc3d8c3b2d720d
huliangjie <lin_032@hotmail.com>
2020-05-27 19:53:37 +0800

    可行

--- Assets/Sample.cs
+++ Assets/Sample.cs
@@ -6,9 +6,19 @@ namespace jsb
     {
         void Awake()
         {
-            var rt = JSBridgeDLL.XJS_NewRuntime();
+            var rt = JSBridgeDLL.JS_NewRuntime();
+            var ctx = JSBridgeDLL.JS_NewContext(rt);
             Debug.Log(rt);
-            JSBridgeDLL.XJS_FreeRuntime(rt);
+            Debug.Log(ctx);
+            var val = JSBridgeDLL.JSB_Test(ctx);
+            Debug.Log(val);
+
+            var jsval = JSBridgeDLL.JS_Eval(ctx, "2+2");
+            int rval;
+            JSBridgeDLL.JS_ToInt32(ctx, out rval, jsval);
+            Debug.LogFormat("2+2={0}", rval);
+            JSBridgeDLL.JS_FreeContext(ctx);
+            JSBridgeDLL.JS_FreeRuntime(rt);
         }
     }
 }
--- Assets/Source/JSBridgeDLL.cs
+++ Assets/Source/JSBridgeDLL.cs
@@ -5,25 +5,50 @@ using AOT;
 
 namespace jsb
 {
+    public struct JSValue
+    {
+        double u; // IntPtr
+        long tag;
+    }
+
     public class JSBridgeDLL
     {
 #if UNITY_IPHONE && !UNITY_EDITOR
 	    const string JSBDLL = "__Internal";
 #else
-        const string JSBDLL = "jsb";
+        const string JSBDLL = "libquickjs";
 #endif
+        [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern IntPtr JS_NewRuntime();
 
         [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr XJS_NewRuntime();
+        public static extern void JS_FreeRuntime(IntPtr rt);
 
         [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern IntPtr XJS_NewContext(IntPtr rt);
+        public static extern IntPtr JS_NewContext(IntPtr rt);
 
         [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void XJS_FreeContext(IntPtr rt);
+        public static extern void JS_FreeContext(IntPtr rt);
 
         [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
-        public static extern void XJS_FreeRuntime(IntPtr rt);
+        public static extern int JS_ToInt32(IntPtr ctx, out int pres, JSValue val);
+        
+        [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern JSValue JS_Eval(IntPtr ctx, byte[] input, ulong input_len, byte[] filename, int eval_flags);
 
+        [DllImport(JSBDLL, CallingConvention = CallingConvention.Cdecl)]
+        public static extern int JSB_Test(IntPtr ctx);
+
+        public static JSValue JS_Eval(IntPtr ctx, string input)
+        {
+            var bytes  = Encoding.UTF8.GetBytes(input);
+            var buf = new byte[bytes.Length + 1];
+            bytes.CopyTo(buf, 0);
+
+            var xx = Encoding.UTF8.GetBytes("main");
+            var nn = new byte[xx.Length + 1];
+            xx.CopyTo(nn, 0);
+            return JS_Eval(ctx, buf, (ulong) bytes.Length, nn, 0);
+        }
     }
 }
