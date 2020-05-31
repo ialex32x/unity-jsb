d16dcc27e45ee805f233360959f00996b3ac1ffd
ialex32x <lin_032@hotmail.com>
2020-05-31 10:07:01 +0800

    mv

---
+++ Assets/jsb/Source/Buffer/ByteBufferPooledAllocator.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+
+namespace QuickJS.IO
+{
+    public class ByteBufferPooledAllocator : ByteBufferAllocator
+    {
+        private int _maxCapacity;
+        private object __mutex = new object();
+        private List<ByteBuffer> __freelist;
+
+        // 预分配缓冲池
+        public ByteBufferPooledAllocator(int prealloc, int initialCapacity, int maxCapacity, bool traceMemoryLeak)
+        {
+            _maxCapacity = maxCapacity;
+            _traceMemoryLeak = traceMemoryLeak;
+
+            lock (__mutex)
+            {
+                __freelist = new List<ByteBuffer>(prealloc);
+                while (prealloc-- > 0)
+                {
+                    __freelist.Add(new ByteBuffer(initialCapacity, maxCapacity, this));
+                }
+            }
+        }
+
+        // 返回一个由对象池分配的 ByteBuffer 对象, 大小至少为 size
+        // Alloc、Release 调用为线程安全的，其余操作需要调用者自己保证线程安全性
+        public override ByteBuffer Alloc(int size)
+        {
+            lock (__mutex)
+            {
+                var count = __freelist.Count;
+                if (count > 0)
+                {
+                    var free = __freelist[count - 1];
+                    __freelist.RemoveAt(count - 1);
+                    free.Retain();
+                    return free;
+                }
+            }
+            return new ByteBuffer(size, _maxCapacity, this).Retain();
+        }
+
+        public override void Recycle(ByteBuffer byteBuffer)
+        {
+            lock (__mutex)
+            {
+                __freelist.Add(byteBuffer);
+            }
+        }
+    }
+}
---
+++ Assets/jsb/Source/Buffer/ByteBufferUnpooledAllocator.cs
@@ -0,0 +1,17 @@
+﻿
+namespace QuickJS.IO
+{
+    public class ByteBufferUnpooledAllocator : ByteBufferAllocator
+    {
+        // 返回一个由对象池分配的 ByteBuffer 对象, 大小至少为 size
+        // Alloc、Release 调用为线程安全的，其余操作需要调用者自己保证线程安全性
+        public override ByteBuffer Alloc(int size)
+        {
+            return new ByteBuffer(size, ByteBufferAllocator.DEFAULT_MAX_CAPACITY, null);
+        }
+
+        public override void Recycle(ByteBuffer byteBuffer)
+        {
+        }
+    }
+}
