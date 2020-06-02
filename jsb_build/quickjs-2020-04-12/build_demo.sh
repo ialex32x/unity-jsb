f3e4f89a9573f90908bcb97383584ad84724bc3d
huliangjie <lin_032@hotmail.com>
2020-06-02 19:55:33 +0800

    build.sh

--- jsb_build/quickjs-2020-04-12/build_demo.sh
+++ jsb_build/quickjs-2020-04-12/build_demo.sh
@@ -5,6 +5,7 @@ make examples/demo.exe
 if [ $? -eq 0 ]; then
     echo "build succeed"
     ./examples/demo.exe
+    cp -f libquickjs.dll ../../Assets/jsb/Plugins/x64/
 else
     echo "build failed"
 fi
