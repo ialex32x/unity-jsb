3118cfce7989e4a10d71915e591bcd77a6355049
huliangjie <lin_032@hotmail.com>
2020-06-11 11:48:12 +0800

    ns.CreateClass

--- Assets/jsb/Source/Native/JSApi+Helpers.cs
+++ Assets/jsb/Source/Native/JSApi+Helpers.cs
@@ -8,8 +8,8 @@ namespace QuickJS.Native
 {
     public partial class JSApi
     {
-        [MonoPInvokeCallback(typeof(JSCFunction))]
-        public static JSValue class_private_ctor(JSContext ctx, JSValue new_target, int argc, JSValue[] argv)
+        [MonoPInvokeCallback(typeof(JSCFunctionMagic))]
+        public static JSValue class_private_ctor(JSContext ctx, JSValue new_target, int argc, JSValue[] argv, int magic)
         {
             JSApi.JS_ThrowInternalError(ctx, "cant call constructor on this type");
             return JSApi.JS_UNDEFINED;
