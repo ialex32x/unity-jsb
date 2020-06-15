ec1bb6b447416789cbe46859c54a7f3a4318687d
huliangjie <lin_032@hotmail.com>
2020-06-15 16:25:46 +0800

    add libwebsockets

---
+++ jsb_build/websockets/make_android.bat
@@ -0,0 +1,41 @@
+@echo off
+
+SET ANDROID_NDK=E:/android-ndk-r15c
+SET BASE_PATH=%~dp0
+SET BUILD_PATH=%~dp0..\out\libwebsockets
+echo %BUILD_PATH%
+
+mkdir %BUILD_PATH% 2>nul
+cd %BUILD_PATH%
+
+rd /s /q %BUILD_PATH%\Android_v7a_release 2>nul
+echo building v7a release
+mkdir %BUILD_PATH%\Android_v7a_release
+cd %BUILD_PATH%\Android_v7a_release
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DANDROID_ABI=armeabi-v7a -DCMAKE_TOOLCHAIN_FILE=%ANDROID_NDK%/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_NATIVE_API_LEVEL=android-9 -G "NMake Makefiles" %BASE_PATH%
+cd %BUILD_PATH%
+cmake --build Android_v7a_release --config Release
+mkdir ..\..\prebuilt\Plugins\Android\libs\armeabi-v7a\
+xcopy /Y .\Android_v7a_release\libwebsockets.so ..\..\prebuilt\Plugins\Android\libs\armeabi-v7a\
+
+rd /s /q %BUILD_PATH%\Android_v8a_release 2>nul
+echo building v8a release
+mkdir %BUILD_PATH%\Android_v8a_release
+cd %BUILD_PATH%\Android_v8a_release
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DANDROID_ABI=arm64-v8a -DCMAKE_TOOLCHAIN_FILE=%ANDROID_NDK%/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=arm-linux-androideabi-clang -DANDROID_NATIVE_API_LEVEL=android-9 -G "NMake Makefiles" %BASE_PATH%
+cd %BUILD_PATH%
+cmake --build Android_v8a_release --config Release
+mkdir ..\..\prebuilt\Plugins\Android\libs\arm64-v8a\
+xcopy /Y .\Android_v8a_release\libwebsockets.so ..\..\prebuilt\Plugins\Android\libs\arm64-v8a\
+
+rd /s /q %BUILD_PATH%\Android_x86_release 2>nul
+echo building x86 release
+mkdir %BUILD_PATH%\Android_x86_release
+cd %BUILD_PATH%\Android_x86_release
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DANDROID_ABI=x86 -DCMAKE_TOOLCHAIN_FILE=%ANDROID_NDK%/build/cmake/android.toolchain.cmake -DANDROID_TOOLCHAIN_NAME=x86-clang -DANDROID_NATIVE_API_LEVEL=android-9 -G "NMake Makefiles" %BASE_PATH%
+cd %BUILD_PATH%
+cmake --build Android_x86_release --config Release
+mkdir ..\..\prebuilt\Plugins\Android\libs\x86\
+xcopy /Y .\Android_x86_release\libwebsockets.so ..\..\prebuilt\Plugins\Android\libs\x86\
+
+cd %BASE_PATH%
---
+++ jsb_build/websockets/make_windows.bat
@@ -0,0 +1,29 @@
+@echo off
+
+SET BASE_PATH=%~dp0
+SET BUILD_PATH=%~dp0..\out\libwebsockets
+echo %BUILD_PATH%
+REM cmake -G "Visual Studio 15 2017 Win64" ..\..\..
+
+mkdir %BUILD_PATH% 2>nul
+cd %BUILD_PATH%
+
+rd /s /q x64_release 2>nul
+echo building x64_release
+mkdir x64_release
+cd x64_release
+cmake -G "Visual Studio 16 2019" -A x64 ..\..\..\websockets
+cd %BUILD_PATH%
+cmake --build x64_release --config Release
+xcopy /Y .\x64_release\Release\libwebsockets.dll ..\..\prebuilt\Plugins\x64\
+
+rd /s /q x86_release 2>nul
+echo building x86_release
+mkdir x86_release
+cd x86_release
+cmake -G "Visual Studio 16 2019" -A Win32 ..\..\..\websockets
+cd %BUILD_PATH%
+cmake --build x86_release --config Release
+xcopy /Y .\x86_release\Release\libwebsockets.dll ..\..\prebuilt\Plugins\x86\
+
+cd %BASE_PATH%
