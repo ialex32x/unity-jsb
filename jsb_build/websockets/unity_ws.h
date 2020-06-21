0292ff240363bf3293cf4112b31c587a3df417c1
ialex32x <lin_032@hotmail.com>
2020-06-21 08:28:38 +0800

    fix unity_ws.h compilation error

--- jsb_build/websockets/unity_ws.h
+++ jsb_build/websockets/unity_ws.h
@@ -4,17 +4,20 @@
 #if defined(WIN32) || defined(_WIN32)
     #define ULWS_EXTERNAL_DECL extern __declspec(dllexport)
     #define ULWS_EXTERNAL __declspec(dllexport)
+#else
+    #define ULWS_EXTERNAL_DECL extern
+    #define ULWS_EXTERNAL
 #endif
 
 typedef int ulws_ssl_type;
 
 ULWS_EXTERNAL_DECL int ulws_pre();
 
-ULWS_EXTERNAL_DECL struct lws_context * ulws_create(const char *name, lws_callback_function callback, size_t rx_buffer_size, size_t tx_packet_size);
+ULWS_EXTERNAL_DECL struct lws_context * ulws_create(const char *name, size_t len, lws_callback_function callback, size_t rx_buffer_size, size_t tx_packet_size);
 
 ULWS_EXTERNAL_DECL void ulws_destroy(struct lws_context *context);
 
-ULWS_EXTERNAL_DECL struct lws *ulws_connect(const struct lws_context *context, 
+ULWS_EXTERNAL_DECL struct lws *ulws_connect(struct lws_context *context, 
                                     const char *protocol_names, 
                                     ulws_ssl_type ssl_type, 
                                     const char *host, const char *address, const char *path, int port);
