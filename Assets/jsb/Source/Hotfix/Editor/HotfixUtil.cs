6630f1039ce262837335f2848ad82801d9e7a539
huliangjie <lin_032@hotmail.com>
2020-07-02 14:44:12 +0800

    in progress

---
+++ Assets/jsb/Source/Hotfix/Editor/HotfixUtil.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace QuickJS.Hotfix
+{
+    using UnityEditor;
+    using UnityEngine;
+
+    public class HotfixUtil
+    {
+        [MenuItem("JS Bridge/Hotfix")]
+        public static void RunHotfix()
+        {
+            Run();
+        }
+
+        private static bool IsHotfixTarget(Mono.Cecil.TypeDefinition td)
+        {
+            foreach (var attr in td.CustomAttributes)
+            {
+                if (attr.AttributeType.FullName == typeof(QuickJS.JSHotfixAttribute).FullName)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void Run()
+        {
+            var test = typeof(HotfixTest);
+            var a = Mono.Cecil.AssemblyDefinition.ReadAssembly(test.Assembly.Location);
+            foreach (var type in a.MainModule.Types)
+            {
+                if (IsHotfixTarget(type))
+                {
+                    Debug.LogFormat("{0}", type.FullName);
+                }
+            }
+        }
+    }
+}
---
+++ jsb_build/hotfix/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace QuickJS.Hotfix
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            HotfixUtil.Run();
+        }
+    }
+}
