47be5b95e3cb3a5ecedca6ba4646c542597b0ee6
huliangjie <lin_032@hotmail.com>
2020-07-09 10:50:38 +0800

    fix include path

--- Assets/jsb/Plugins/WebGL/libquickjs.c
+++ Assets/jsb/Plugins/WebGL/libquickjs.c
@@ -1,7 +1,7 @@
 
-#include "../../../jsb_build/quickjs/quickjs-2020-04-12/quickjs.c"
-#include "../../../jsb_build/quickjs/quickjs-2020-04-12/libregexp.c"
-#include "../../../jsb_build/quickjs/quickjs-2020-04-12/libunicode.c"
-#include "../../../jsb_build/quickjs/quickjs-2020-04-12/cutils.c"
-#include "../../../jsb_build/quickjs/quickjs-2020-04-12/libbf.c"
-#include "../../../jsb_build/quickjs/unity_qjs.c"
+#include "../../../../jsb_build/quickjs/quickjs-2020-04-12/quickjs.c"
+#include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libregexp.c"
+#include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libunicode.c"
+#include "../../../../jsb_build/quickjs/quickjs-2020-04-12/cutils.c"
+#include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libbf.c"
+#include "../../../../jsb_build/quickjs/unity_qjs.c"
