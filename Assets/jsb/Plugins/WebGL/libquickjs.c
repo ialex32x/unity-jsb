954cd6e1c6c4f6a07f33f49e310d9ca45867da65
huliangjie <lin_032@hotmail.com>
2020-07-10 11:46:03 +0800

    update libquickjs.c

--- Assets/jsb/Plugins/WebGL/libquickjs.c
+++ Assets/jsb/Plugins/WebGL/libquickjs.c
@@ -1,6 +1,6 @@
 
 #include "../../../../jsb_build/quickjs/quickjs-2020-04-12/quickjs.c"
-#include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libregexp.c"
+// #include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libregexp.c"
 #include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libunicode.c"
 #include "../../../../jsb_build/quickjs/quickjs-2020-04-12/cutils.c"
 #include "../../../../jsb_build/quickjs/quickjs-2020-04-12/libbf.c"
