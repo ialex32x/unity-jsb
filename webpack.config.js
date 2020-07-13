785fab9cf0c1b4252b99a9df58b74acfd97eb8cc
huliangjie <lin_032@hotmail.com>
2020-07-13 17:58:08 +0800

    调整配置使 webpack 可以正常打包

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,25 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: {
-        index: './Scripts/out/main.js'
+        index: './Assets/Examples/Scripts/src/main.ts'
     },
     output: {
         filename: 'main.js',
-        path: path.resolve(__dirname, './Assets/Examples/Scripts/')
+        path: path.resolve(__dirname, './Assets/Examples/Scripts/dist')
     },
-    mode: "development", 
+    module: {
+        rules: [{
+            test: /\.tsx?$/, 
+            parser: { amd: false, system: false } ,
+            use: 'ts-loader', 
+            // use: 'awesome-typescript-loader', 
+            exclude: /node_modules/
+        }]
+    }, 
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    mode: 'development', 
     plugins: [
         new CleanWebpackPlugin()
     ]
