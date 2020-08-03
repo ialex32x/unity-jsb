9f6a9cad09c20904266600fdc4541f16d2c66555
huliangjie <lin_032@hotmail.com>
2020-08-03 15:28:35 +0800

    add log

--- Assets/Examples/Source/Sample.cs
+++ Assets/Examples/Source/Sample.cs
@@ -1,3 +1,4 @@
+using System;
 using QuickJS;
 using QuickJS.Binding;
 using QuickJS.Utils;
@@ -9,22 +10,140 @@ namespace jsb
 
     public class Sample : MonoBehaviour, IScriptRuntimeListener
     {
+        public class TextUnityLogger : IScriptLogger
+        {
+            private UnityEngine.UI.Text _text;
+
+            public TextUnityLogger(UnityEngine.UI.Text text)
+            {
+                _text = text;
+            }
+
+            public void Error(Exception exception)
+            {
+                LogException(exception);
+            }
+
+            public void Write(LogLevel ll, string text)
+            {
+                switch (ll)
+                {
+                    case LogLevel.Info: Log(text); return;
+                    case LogLevel.Warn: LogWarning(text); return;
+                    case LogLevel.Error: LogError(text); return;
+                    default: LogError(text); return;
+                }
+            }
+
+            public void Write(LogLevel ll, string fmt, params object[] args)
+            {
+                switch (ll)
+                {
+                    case LogLevel.Info: LogFormat(fmt, args); return;
+                    case LogLevel.Warn: LogWarningFormat(fmt, args); return;
+                    case LogLevel.Error: LogErrorFormat(fmt, args); return;
+                    default: LogErrorFormat(fmt, args); return;
+                }
+            }
+
+            public void ScriptWrite(LogLevel ll, string text)
+            {
+                switch (ll)
+                {
+                    case LogLevel.Info: Log(text); return;
+                    case LogLevel.Warn: LogWarning(text); return;
+                    case LogLevel.Error: LogError(text); return;
+                    default: LogError(text); return;
+                }
+            }
+
+            public void ScriptWrite(LogLevel ll, string fmt, params object[] args)
+            {
+                switch (ll)
+                {
+                    case LogLevel.Info: LogFormat(fmt, args); return;
+                    case LogLevel.Warn: LogWarningFormat(fmt, args); return;
+                    case LogLevel.Error: LogErrorFormat(fmt, args); return;
+                    default: LogErrorFormat(fmt, args); return;
+                }
+            }
+
+            private void LogError(string text)
+            {
+                if (_text != null)
+                {
+                    _text.text += text;
+                }
+            }
+
+            private void LogErrorFormat(string fmt, object[] args)
+            {
+                if (_text != null)
+                {
+                    _text.text += string.Format(fmt, args);
+                }
+            }
+
+            private void LogWarningFormat(string fmt, object[] args)
+            {
+                if (_text != null)
+                {
+                    _text.text += string.Format(fmt, args);
+                }
+            }
+
+            private void LogFormat(string fmt, object[] args)
+            {
+                if (_text != null)
+                {
+                    _text.text += string.Format(fmt, args);
+                }
+            }
+
+            private void LogException(Exception exception)
+            {
+                if (_text != null)
+                {
+                    _text.text += exception;
+                }
+            }
+
+            private void Log(string text)
+            {
+                if (_text != null)
+                {
+                    _text.text += text;
+                }
+            }
+
+            private void LogWarning(string text)
+            {
+                if (_text != null)
+                {
+                    _text.text += text;
+                }
+            }
+        }
+
         public enum FileLoader
         {
             Default,
             Resources,
             HMR,
         }
+        public UnityEngine.UI.Text text;
         public FileLoader fileLoader;
         public string baseUrl = "http://127.0.0.1:8182";
         public bool sourceMap;
         public bool stacktrace;
         private ScriptRuntime _rt;
+        private TextUnityLogger _tul;
 
         void Awake()
         {
             IFileSystem fileSystem;
 
+            _tul = new TextUnityLogger(text);
             _rt = ScriptEngine.CreateRuntime();
             _rt.AddSearchPath("node_modules");
 
@@ -50,7 +169,8 @@ namespace jsb
             {
                 _rt.EnableSourceMap();
             }
-            _rt.Initialize(fileSystem, this);
+            _tul.Write(LogLevel.Info, "Init");
+            _rt.Initialize(fileSystem, this, _tul, new ByteBufferPooledAllocator());
         }
 
         void Update()
@@ -65,14 +185,17 @@ namespace jsb
 
         public void OnBind(ScriptRuntime runtime, TypeRegister register)
         {
+            _tul.Write(LogLevel.Info, "Bind");
             QuickJS.Extra.WebSocket.Bind(register);
             QuickJS.Extra.XMLHttpRequest.Bind(register);
             QuickJS.Extra.DOMCompatibleLayer.Bind(register);
             QuickJS.Extra.NodeCompatibleLayer.Bind(register);
+            _tul.Write(LogLevel.Info, "Bind Finish");
         }
 
         public void OnComplete(ScriptRuntime runtime)
         {
+            _tul.Write(LogLevel.Info, "run");
             _rt.EvalMain("main.js");
         }
     }
