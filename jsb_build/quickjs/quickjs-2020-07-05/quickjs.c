1cbe7f5e28a627d659832c0612797e850158820f
huliangjie <lin_032@hotmail.com>
2020-09-01 18:59:02 +0800

    fix CLOCK_REALTIME (try)

--- jsb_build/quickjs/quickjs-2020-07-05/quickjs.c
+++ jsb_build/quickjs/quickjs-2020-07-05/quickjs.c
@@ -981,6 +981,25 @@ enum OPCodeEnum {
 static double log2(double v) { return log(v) / log(2.0); }
 #endif
 
+#if defined(__APPLE__)
+// https://stackoverflow.com/questions/5167269/clock-gettime-alternative-in-mac-os-x
+// not best solution
+#ifndef CLOCK_REALTIME
+#define CLOCK_REALTIME 0
+int clock_gettime(int clk_id, struct timespec *t){
+    mach_timebase_info_data_t timebase;
+    mach_timebase_info(&timebase);
+    uint64_t time;
+    time = mach_absolute_time();
+    double nseconds = ((double)time * (double)timebase.numer)/((double)timebase.denom);
+    double seconds = ((double)time * (double)timebase.numer)/((double)timebase.denom * 1e9);
+    t->tv_sec = seconds;
+    t->tv_nsec = nseconds;
+    return 0;
+}
+#endif
+#endif
+
 static int JS_InitAtoms(JSRuntime *rt);
 static JSAtom __JS_NewAtomInit(JSRuntime *rt, const char *str, int len,
                                int atom_type);
