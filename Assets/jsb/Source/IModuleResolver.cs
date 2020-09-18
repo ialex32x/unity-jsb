e2433a75aca5d2d040fa62738bf8fdfa2aa87eee
huliangjie <lin_032@hotmail.com>
2020-09-18 18:57:40 +0800

    memo

--- Assets/jsb/Source/IModuleResolver.cs
+++ Assets/jsb/Source/IModuleResolver.cs
@@ -4,8 +4,31 @@ namespace QuickJS
 {
     using Native;
 
+    /*
+foreach (var r in list)
+{
+    JSValue mod;
+    string id;
+    if (r.ResolveModule(p, m, out id))
+    {
+        if (LoadCache(id, out mod))
+        {
+            return mod;
+        }
+
+        if (r.LoadModule(id, out mod))
+        {
+
+        }
+
+        return error;
+    }
+}
+return error;
+    */
     public interface IModuleResolver
     {
-        bool Resolve(string parent_module_id, string module_id, out string resolved_id, out JSValue mod_obj);
+        bool ResolveModule(string parent_module_id, string module_id, out string resolved_id);
+        JSValue LoadModule(string resolved_id);
     }
 }
