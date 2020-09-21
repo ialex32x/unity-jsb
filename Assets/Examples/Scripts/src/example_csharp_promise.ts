c9949aa1174fc4a1035712a3d4fd31a99b7a51e6
huliangjie <lin_032@hotmail.com>
2020-09-21 14:36:58 +0800

    update examples

--- Assets/Examples/Scripts/src/example_csharp_promise.ts
+++ Assets/Examples/Scripts/src/example_csharp_promise.ts
@@ -1,9 +1,10 @@
 
 async function test_custom_promise() {
-    print("想象3秒后弹出的是个对话框")
+    print("想象3秒后弹出的是个对话框");
     await jsb.Yield(new UnityEngine.WaitForSeconds(3));
     let go = new UnityEngine.GameObject("custome.promise.test");
     var sb = go.AddComponent(SampleBehaviour);
+    print("想象这里是等待用户点击对话框按钮");
     print("完成 (promise in C#):", await sb.SimpleWait(1));
 }
 
