a0576ab7b6bb39744aeceafca3d61bacc984d0ae
huliangjie <lin_032@hotmail.com>
2020-09-28 15:53:29 +0800

    add FairyGUI extra

---
+++ Assets/jsb/Extra/FairyGUI/Editor/FairyGUICustomBinding.cs
@@ -0,0 +1,116 @@
+using System.Reflection;
+
+namespace QuickJS.Extra
+{
+    using QuickJS.Unity;
+    using UnityEngine;
+
+    public class FairyGUICustomBinding : AbstractBindingProcess
+    {
+        public override void OnPreCollectAssemblies(BindingManager bindingManager)
+        {
+            bindingManager.AddTypePrefixBlacklist("WebSocketSharp.");
+        }
+
+        public override void OnPreExporting(BindingManager bindingManager)
+        {
+            bindingManager.AddExportedType(typeof(FairyGUI.ProgressTitleType));
+            bindingManager.AddExportedType(typeof(FairyGUI.PackageItemType));
+            bindingManager.AddExportedType(typeof(FairyGUI.ObjectType));
+            bindingManager.AddExportedType(typeof(FairyGUI.RelationType));
+            bindingManager.AddExportedType(typeof(FairyGUI.GroupLayoutType));
+            bindingManager.AddExportedType(typeof(FairyGUI.ChildrenRenderOrder));
+            bindingManager.AddExportedType(typeof(FairyGUI.FlipType));
+            bindingManager.AddExportedType(typeof(FairyGUI.FillMethod));
+            bindingManager.AddExportedType(typeof(FairyGUI.ListLayoutType));
+            bindingManager.AddExportedType(typeof(FairyGUI.CustomEase));
+            bindingManager.AddExportedType(typeof(FairyGUI.GPathPoint));
+            bindingManager.AddExportedType(typeof(FairyGUI.NTexture));
+            bindingManager.AddExportedType(typeof(FairyGUI.Utils.ByteBuffer));
+            bindingManager.AddExportedType(typeof(FairyGUI.OverflowType));
+            bindingManager.AddExportedType(typeof(FairyGUI.FillType));
+            bindingManager.AddExportedType(typeof(FairyGUI.PixelHitTestData));
+            bindingManager.AddExportedType(typeof(FairyGUI.BitmapFont));
+            bindingManager.AddExportedType(typeof(FairyGUI.TransitionActionType));
+            bindingManager.AddExportedType(typeof(FairyGUI.AutoSizeType));
+            bindingManager.AddExportedType(typeof(FairyGUI.TweenPropType));
+            bindingManager.AddExportedType(typeof(FairyGUI.ScrollType));
+            bindingManager.AddExportedType(typeof(FairyGUI.ScrollBarDisplayType));
+            bindingManager.AddExportedType(typeof(FairyGUI.ButtonMode));
+            bindingManager.AddExportedType(typeof(FairyGUI.AlignType));
+            bindingManager.AddExportedType(typeof(FairyGUI.VertAlignType));
+            bindingManager.AddExportedType(typeof(FairyGUI.OriginHorizontal));
+            bindingManager.AddExportedType(typeof(FairyGUI.OriginVertical));
+            bindingManager.AddExportedType(typeof(FairyGUI.ListSelectionMode));
+            bindingManager.AddExportedType(typeof(FairyGUI.PopupDirection));
+            bindingManager.AddExportedType(typeof(FairyGUI.NAudioClip));
+            bindingManager.AddExportedType(typeof(FairyGUI.UIContentScaler));
+            bindingManager.AddExportedType(typeof(FairyGUI.UIContentScaler.ScaleMode));
+            bindingManager.AddExportedType(typeof(FairyGUI.UIContentScaler.ScreenMatchMode));
+            bindingManager.AddExportedType(typeof(FairyGUI.EventContext));
+            bindingManager.AddExportedType(typeof(FairyGUI.EventDispatcher));
+            bindingManager.AddExportedType(typeof(FairyGUI.EventListener));
+            bindingManager.AddExportedType(typeof(FairyGUI.GScrollBar));
+            bindingManager.AddExportedType(typeof(FairyGUI.GPath));
+            bindingManager.AddExportedType(typeof(FairyGUI.InputEvent));
+            bindingManager.AddExportedType(typeof(FairyGUI.DisplayObject));
+            bindingManager.AddExportedType(typeof(FairyGUI.Container))
+                .AddTSMethodDeclaration("InvalidateBatchingState()");
+            bindingManager.AddExportedType(typeof(FairyGUI.Stage));
+            bindingManager.AddExportedType(typeof(FairyGUI.Controller));
+            bindingManager.AddExportedType(typeof(FairyGUI.GObject));
+            bindingManager.AddExportedType(typeof(FairyGUI.GGraph));
+            bindingManager.AddExportedType(typeof(FairyGUI.GGroup));
+            bindingManager.AddExportedType(typeof(FairyGUI.GImage));
+            bindingManager.AddExportedType(typeof(FairyGUI.GLoader));
+            bindingManager.AddExportedType(typeof(FairyGUI.GMovieClip));
+            bindingManager.AddExportedType(typeof(FairyGUI.Utils.XMLList));
+            bindingManager.AddExportedType(typeof(FairyGUI.Image));
+            bindingManager.AddExportedType(typeof(FairyGUI.BlendMode));
+            bindingManager.AddExportedType(typeof(FairyGUI.GearXY));
+            bindingManager.AddExportedType(typeof(FairyGUI.GearLook));
+            bindingManager.AddExportedType(typeof(FairyGUI.GearSize));
+            bindingManager.AddExportedType(typeof(FairyGUI.GLoader3D));
+            bindingManager.AddExportedType(typeof(FairyGUI.GTree));
+            bindingManager.AddExportedType(typeof(FairyGUI.GTreeNode));
+            bindingManager.AddExportedType(typeof(FairyGUI.MovieClip));
+            bindingManager.AddExportedType(typeof(FairyGUI.MovieClip.Frame));
+            bindingManager.AddExportedType(typeof(FairyGUI.TextFormat));
+            bindingManager.AddExportedType(typeof(FairyGUI.GTextField));
+            bindingManager.AddExportedType(typeof(FairyGUI.GRichTextField));
+            bindingManager.AddExportedType(typeof(FairyGUI.GTextInput));
+            bindingManager.AddExportedType(typeof(FairyGUI.GComponent))
+                .AddTSMethodDeclaration("InvalidateBatchingState()");
+            bindingManager.AddExportedType(typeof(FairyGUI.GList))
+                .AddTSMethodDeclaration("RemoveChildAt(index: number): FairyGUI.GObject");
+            bindingManager.AddExportedType(typeof(FairyGUI.GRoot));
+            bindingManager.AddExportedType(typeof(FairyGUI.GLabel));
+            bindingManager.AddExportedType(typeof(FairyGUI.GButton));
+            bindingManager.AddExportedType(typeof(FairyGUI.GComboBox));
+            bindingManager.AddExportedType(typeof(FairyGUI.GProgressBar));
+            bindingManager.AddExportedType(typeof(FairyGUI.GSlider));
+            bindingManager.AddExportedType(typeof(FairyGUI.PopupMenu));
+            bindingManager.AddExportedType(typeof(FairyGUI.ScrollPane));
+            bindingManager.AddExportedType(typeof(FairyGUI.Transition));
+            bindingManager.AddExportedType(typeof(FairyGUI.UIPackage));
+            bindingManager.AddExportedType(typeof(FairyGUI.Window));
+            bindingManager.AddExportedType(typeof(FairyGUI.GObjectPool));
+            bindingManager.AddExportedType(typeof(FairyGUI.Relations));
+            bindingManager.AddExportedType(typeof(FairyGUI.Timers));
+            bindingManager.AddExportedType(typeof(FairyGUI.GTween));
+            bindingManager.AddExportedType(typeof(FairyGUI.GTweener));
+            bindingManager.AddExportedType(typeof(FairyGUI.EaseType));
+            bindingManager.AddExportedType(typeof(FairyGUI.TweenValue));
+            bindingManager.AddExportedType(typeof(FairyGUI.PackageItem));
+            bindingManager.AddExportedType(typeof(FairyGUI.UIObjectFactory));
+            bindingManager.AddExportedType(typeof(FairyGUI.Utils.XML));
+            bindingManager.AddExportedType(typeof(UnityEngine.AudioClip));
+            bindingManager.AddExportedType(typeof(UnityEngine.AssetBundle));
+            bindingManager.AddExportedType(typeof(System.Collections.Generic.IList<FairyGUI.GObject>));
+            bindingManager.AddExportedType(typeof(System.Collections.Generic.List<FairyGUI.PackageItem>));
+            bindingManager.AddExportedType(typeof(System.Collections.Generic.List<FairyGUI.UIPackage>));
+            bindingManager.AddExportedType(typeof(System.Collections.Generic.List<int>));
+            bindingManager.AddExportedType(typeof(System.Collections.Generic.List<string>));
+        }
+    }
+}
