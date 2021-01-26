abe2bd20e68696e1c8a035aa2b867b122437c2d0
huliangjie <lin_032@hotmail.com>
2021-01-26 12:33:00 +0800

    整理 example_ 脚本, 排除暂时不支持的特性

--- Scripts/out/example_ref_out.js
+++ Scripts/out/example_ref_out.js
@@ -1,24 +1,33 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const global_1 = require("global");
-if (module == require.main) {
-    let g = { value: 1 };
-    let x = {};
-    let z = {};
-    global_1.NoNamespaceClass.TestRefOut(g, x, 666, z);
-    print("ref/out:", g.value, x.value, z.value);
+const jsb = require("jsb");
+function execute() {
+    if (module == require.main) {
+        let g = { value: 1 };
+        let x = {};
+        let z = {};
+        global_1.NoNamespaceClass.TestRefOut(g, x, 666, z);
+        print("ref/out:", g.value, x.value, z.value);
+    }
+    // 可以通过指定 type 提高重载匹配的有效性
+    if (module == require.main) {
+        let x = { type: Number };
+        let z = { type: Number };
+        global_1.NoNamespaceClass.TestOut(x, 233, z);
+        print("out:", x.value, typeof x.value, z.value, typeof z.value);
+    }
+    if (module == require.main) {
+        let x = { type: String };
+        let z = { type: String };
+        global_1.NoNamespaceClass.TestOut(x, 999, z);
+        print("out:", x.value, typeof x.value, z.value, typeof z.value);
+    }
 }
-// 可以通过指定 type 提高重载匹配的有效性
-if (module == require.main) {
-    let x = { type: Number };
-    let z = { type: Number };
-    global_1.NoNamespaceClass.TestOut(x, 233, z);
-    print("out:", x.value, typeof x.value, z.value, typeof z.value);
+if (!jsb.IsReflectBind()) {
+    execute();
 }
-if (module == require.main) {
-    let x = { type: String };
-    let z = { type: String };
-    global_1.NoNamespaceClass.TestOut(x, 999, z);
-    print("out:", x.value, typeof x.value, z.value, typeof z.value);
+else {
+    console.warn("reflectbind 模式下暂时不支持引用参数");
 }
 //# sourceMappingURL=example_ref_out.js.map
--- Scripts/src/example_xhr.ts
+++ Scripts/src/example_xhr.ts
@@ -1,7 +1,7 @@
 
 let xhr = new XMLHttpRequest();
-xhr.open("GET", "http://127.0.0.1:8080/windows/checksum.txt");
-// xhr.open("GET", "https://www.baidu.com");
+// xhr.open("GET", "http://127.0.0.1:8080/windows/checksum.txt");
+xhr.open("GET", "https://www.baidu.com");
 xhr.timeout = 3000;
 xhr.onerror = function (e) {
     console.error(e);
