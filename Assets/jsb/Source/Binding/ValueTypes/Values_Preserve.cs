881e6647558a61240769b7de69946506aa729eea
huliangjie <lin_032@hotmail.com>
2021-01-26 10:28:49 +0800

    class Values 添加 Preserve 属性

---
+++ Assets/jsb/Source/Binding/ValueTypes/Values_Preserve.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
+namespace QuickJS.Binding
+{
+    using UnityEngine;
+    using Native;
+
+    [UnityEngine.Scripting.Preserve]
+    public partial class Values
+    {
+    }
+}
