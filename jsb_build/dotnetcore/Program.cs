f79b8db23a50ca61ae6302556526c70ebae01366
ialex32x <lin_032@hotmail.com>
2021-04-30 09:49:08 +0800

    add dotnetcore sample project (jsb_build/dotnetcore/example.csproj)

---
+++ jsb_build/dotnetcore/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading;
+using QuickJS;
+using QuickJS.IO;
+using QuickJS.Utils;
+
+namespace DotnetCoreConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new DefaultScriptLogger();
+            var fileResolver = new PathResolver();
+            var fileSystem = new DefaultFileSystem(logger);
+            var asyncManager = new DefaultAsyncManager();
+            var runtime = ScriptEngine.CreateRuntime();
+
+            runtime.AddModuleResolvers();
+            runtime.Initialize(fileSystem, fileResolver, asyncManager, logger, new ByteBufferPooledAllocator(), ReflectionBinder.GetBinder(true));
+            runtime.AddSearchPath("./");
+            runtime.AddSearchPath("./node_modules");
+            runtime.EvalMain("main");
+            while (runtime.isRunning)
+            {
+                runtime.Update(1);
+                Thread.Sleep(1);
+            }
+            runtime.Shutdown();
+        }
+    }
+}
