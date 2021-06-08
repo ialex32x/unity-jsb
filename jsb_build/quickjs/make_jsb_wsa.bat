d418154061740ff81d2fcbd8a8c53b458ed9cde3
huliangjie <lin_032@hotmail.com>
2021-06-08 14:51:19 +0800

    Update make_jsb_wsa.bat

--- jsb_build/quickjs/make_jsb_wsa.bat
+++ jsb_build/quickjs/make_jsb_wsa.bat
@@ -11,12 +11,42 @@ echo %VS_VERSION%
 mkdir %BUILD_PATH% 2>nul
 cd %BUILD_PATH%
 
+rd /s /q %BUILD_PATH%\wsa_x86_release 2>nul
+echo building wsa_x86_release
+mkdir %BUILD_PATH%\wsa_x86_release
+cd %BUILD_PATH%\wsa_x86_release
+cmake -DQJS_VERSION=2021-03-27 -DCONFIG_LTO=y -DCONFIG_WSA=y -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -G %VS_VERSION% -A Win32 %BASE_PATH%
+cd %BUILD_PATH%
+cmake --build wsa_x86_release --config Release
+mkdir ..\..\prebuilt\Plugins\WSA\x86\
+xcopy /Y .\wsa_x86_release\Release\quickjs.dll ..\..\prebuilt\Plugins\WSA\x86\
+
 rd /s /q %BUILD_PATH%\wsa_x64_release 2>nul
 echo building wsa_x64_release
 mkdir %BUILD_PATH%\wsa_x64_release
 cd %BUILD_PATH%\wsa_x64_release
-cmake -DQJS_VERSION=2021-03-27 -DCONFIG_LTO=y -DCONFIG_WSA=y -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -G %VS_VERSION% %BASE_PATH%
+cmake -DQJS_VERSION=2021-03-27 -DCONFIG_LTO=y -DCONFIG_WSA=y -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -G %VS_VERSION% -A x64 %BASE_PATH%
 cd %BUILD_PATH%
 cmake --build wsa_x64_release --config Release
 mkdir ..\..\prebuilt\Plugins\WSA\x64\
-xcopy /Y .\wsa_x64_release\Releasa\quickjs.dll ..\..\prebuilt\Plugins\WSA\x64\
+xcopy /Y .\wsa_x64_release\Release\quickjs.dll ..\..\prebuilt\Plugins\WSA\x64\
+
+rd /s /q %BUILD_PATH%\wsa_ARM_release 2>nul
+echo building wsa_ARM_release
+mkdir %BUILD_PATH%\wsa_ARM_release
+cd %BUILD_PATH%\wsa_ARM_release
+cmake -DQJS_VERSION=2021-03-27 -DCONFIG_LTO=y -DCONFIG_WSA=y -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -G %VS_VERSION% -A ARM %BASE_PATH%
+cd %BUILD_PATH%
+cmake --build wsa_ARM_release --config Release
+mkdir ..\..\prebuilt\Plugins\WSA\ARM\
+xcopy /Y .\wsa_ARM_release\Release\quickjs.dll ..\..\prebuilt\Plugins\WSA\ARM\
+
+rd /s /q %BUILD_PATH%\wsa_ARM64_release 2>nul
+echo building wsa_ARM64_release
+mkdir %BUILD_PATH%\wsa_ARM64_release
+cd %BUILD_PATH%\wsa_ARM64_release
+cmake -DQJS_VERSION=2021-03-27 -DCONFIG_LTO=y -DCONFIG_WSA=y -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_SYSTEM_NAME=WindowsStore -DCMAKE_SYSTEM_VERSION=10.0 -G %VS_VERSION% -A ARM64 %BASE_PATH%
+cd %BUILD_PATH%
+cmake --build wsa_ARM64_release --config Release
+mkdir ..\..\prebuilt\Plugins\WSA\ARM64\
+xcopy /Y .\wsa_ARM64_release\Release\quickjs.dll ..\..\prebuilt\Plugins\WSA\ARM64\
