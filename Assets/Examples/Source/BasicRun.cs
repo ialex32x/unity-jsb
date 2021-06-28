c8a67690833654d70568a54c4ad92f3c93191662
ialex32x <lin_032@hotmail.com>
2021-06-28 10:14:29 +0800

    update BasicRun

--- Assets/Examples/Source/BasicRun.cs
+++ Assets/Examples/Source/BasicRun.cs
@@ -15,18 +15,18 @@ namespace Example
             if (execRuntime)
             {
                 var rt = JSApi.JS_NewRuntime();
-                // JSMemoryUsage s;
-                // JSApi.JS_ComputeMemoryUsage(rt, &s);
-                // Debug.Log($"test {rt}: {s.malloc_count} {s.malloc_size} {s.malloc_limit}");
-                // var ctx = JSApi.JS_NewContextRaw(rt);
-                // Debug.Log("test:" + ctx);
+                JSMemoryUsage s;
+                JSApi.JS_ComputeMemoryUsage(rt, &s);
+                Debug.Log($"test {rt}: {s.malloc_count} {s.malloc_size} {s.malloc_limit}");
+                var ctx = JSApi.JS_NewContext(rt);
+                Debug.Log("test:" + ctx);
 
                 // // // JSApi.JS_AddIntrinsicOperators(ctx);
                 // // var obj = JSApi.JS_NewObject(ctx);
                 // // JSApi.JS_FreeValue(ctx, obj);
 
-                // JSApi.JS_FreeContext(ctx);
-                // JSApi.JS_FreeRuntime(rt);
+                JSApi.JS_FreeContext(ctx);
+                JSApi.JS_FreeRuntime(rt);
                 Debug.Log("it's a good day");
             }
         }
